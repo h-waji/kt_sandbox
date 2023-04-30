5d3770323a57947e1bc0531aac180dae848f00d4
wajima <goo.hiro.w@gmail.com>
2023-04-30 20:56:34 +0900

    variable

---
+++ src/01_variable.kt
@@ -0,0 +1,10 @@
+// fun main(args: Array<String>) {
+fun main() {
+  // val msg: String = "Hello World"
+  // val msg = "Hello World"
+  var msg = "Hello World"
+  println(msg)
+
+  msg = "Hello 2"
+  println(msg)
+}
