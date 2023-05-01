3678dbde9d6baa5cc60068a75f234a50618b2f7e
wajima <goo.hiro.w@gmail.com>
2023-05-01 20:55:51 +0900

    type

---
+++ src/02_type.kt
@@ -0,0 +1,26 @@
+
+
+fun main() {
+  // String, Char
+  val msg: String = "Hoge"
+  println(msg)
+
+  val c: Char = 'a'
+  println(c)
+
+  // Byte, Short, Int, Long
+  val i: Int = 2023
+  println(i.toString())
+  val l: Long = 9999999999L
+  println(l.toString())
+
+  // Float, Double
+  val d: Double = 20.23
+  val f: Float = 123.45F
+  println(d.toString())
+  println(f)
+
+  // Boolean
+  val flag: Boolean = false
+  println(flag)
+}
