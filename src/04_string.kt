003dc331bd679319d3d338a2b41d915ca88d36a0
wajima <goo.hiro.w@gmail.com>
2023-05-01 21:10:48 +0900

    string

---
+++ src/04_string.kt
@@ -0,0 +1,11 @@
+fun main() {
+  println("Hello " + "world")
+
+  var name = "Miku"
+  println("My name is $name")
+
+  println("My score is ${20 + 23}")
+
+  println("Hello\nwor\tld")
+  println("Hello\\nwor\\tld")
+}
