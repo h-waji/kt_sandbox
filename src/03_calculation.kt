5b639c803ddfcbb19986debfaa0ccda92029f4bf
wajima <goo.hiro.w@gmail.com>
2023-05-01 21:04:29 +0900

    calculation

---
+++ src/03_calculation.kt
@@ -0,0 +1,19 @@
+fun main() {
+  val x = 10
+  println(x / 3)
+  println(x / 3.0)
+  println(x % 3)
+
+  var y = 5
+  y++
+  println(y)
+  y--
+  println(y)
+
+  var z = 4
+  z += 16
+  println(z)
+
+  var flag = true
+  println(!flag)
+}
